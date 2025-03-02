23122cd1ed7f40c83d43050e010cd2f4fb9d30f3
dominionism <cainglet@uw.edu>
2025-03-02 00:37:05 -0800

    Created the React application.

---
+++ yearly-activity-report/src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+export default function Navigation() {
+    return (
+        <nav className="nav-bar">
+            <ul>
+                <li><i className="fa-solid fa-house"></i>Dashboard</li>
+                <li><i className="fa-solid fa-file"></i>YAR</li>
+                <li><i className="fa-solid fa-address-card"></i>Faculty</li>
+                <li><i className="fa-solid fa-folder-open"></i>Files</li>
+                <hr></hr>
+                <li><i class="fa-solid fa-handshake-angle"></i>Help Center</li>
+                <li><i class="fa-solid fa-shield-halved"></i>Privacy Statement</li>
+            </ul>
+        </nav>
+    )
+}
