5795d9c11e8d4148237b2ed6dff980c2ea95cc51
dominionism <cainglet@uw.edu>
2025-04-04 05:40:18 -0700

    Completed the skeleton of the chair view.

--- yearly-activity-report/src/App.jsx
+++ yearly-activity-report/src/App.jsx
@@ -1,8 +1,10 @@
 import Navigation from "./components/Navigation"
+import Visualization from "./components/Visualization"
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Visualization/>
     </div>
   )
 }
---
+++ yearly-activity-report/src/components/Visualization.jsx
@@ -0,0 +1,33 @@
+import dummyOne from '../images/dummy.png';
+import dummyTwo from '../images/dummyTwo.png';
+
+export default function Visualization(props) {
+    return (
+        <div className="visual-container">
+            <h1 className="username">Welcome Bill Howe</h1>
+            <div className="visual-generator">
+                <h2>Visualization Type</h2>
+                <select className="visualization-dropdown">
+                    <option value="" disabled selected>Select Type</option>
+                </select>
+                <textarea className="visualization-prompt" placeholder="Describe visualization, include x and y values!"></textarea>
+                <div className="visual-buttons-container">
+                    <button className="cancelButton">Cancel</button>
+                    <button className="generateButton">Generate</button>
+                </div>
+            </div>
+            <div className="visual-section">
+                <h2>Publication Visualization from 2020-2024</h2>
+                <div className="image-container">
+                    <img src={dummyOne} alt="Publication Visualization" />
+                </div>
+            </div>
+            <div className="publication-distribution">
+                <h2>Publication Distribution</h2>
+                <div className="image-container">
+                    <img src={dummyTwo} alt="Publication Distribution" />
+                </div>
+            </div>
+        </div>
+    )
+}
