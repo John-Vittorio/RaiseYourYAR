d4c6105d14e83590fc5b43b6f1342d7290af714b
dominionism <cainglet@uw.edu>
2025-04-20 17:09:52 -0700

    Provided additional notes for the backend.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-dotenv.config();
 const app = express();
 
+/* Middleware */
+/* This middleware allows us to accept and parse JSON data from req.body. */
+app.use(express.json());
+
+/* Important Note: Make sure to import all models */
+/* Model Imports Section */
+
+/* Provides access to the variables in the .env file. */
+dotenv.config();
+
 app.get("/report", (req, res) => {
-    res.send("Server is running...")
+    res.send("Report Endpoint Active")
 })
+
+/* 
+POST Handler
+*/
 
 app.listen(5000, () => {
     connectDB();
