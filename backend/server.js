6a5ed80ad99110d41b9ba9c6c05188775d8f0d18
dominionism <cainglet@uw.edu>
2025-04-28 15:33:07 -0700

    priming for deployment.

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 /* Middleware */
 app.use(express.json());
 app.use(cors({
-    origin: ['*', 'http://localhost:5173', 'http://localhost:3000', 'http://localhost:5001', 'http://localhost:5000'],
+    origin: ['*', 'http://localhost:5173', 'http://localhost:3000', 'http://localhost:5001', 'http://localhost:5000', 'yearlyactivityreport.netlify.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
