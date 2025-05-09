7873fa9cb5c92117a7b44b585b43bbd90c217770
dominionism <cainglet@uw.edu>
2025-05-09 02:01:25 -0700

    Trying.

--- frontend/src/components/ReportViewPage.jsx
+++ frontend/src/components/ReportViewPage.jsx
@@ -75,7 +75,7 @@ const ReportViewPage = () => {
               >
                 <path d="M19 12H5M12 19l-7-7 7-7"/>
               </svg>
-              Back to Main
+              Back
             </button>
           </div>
           
