dcb9799ce5633d9ca7e8e84a823384e86cd6a3ec
dominionism <cainglet@uw.edu>
2025-05-15 21:02:41 -0700

    .

--- frontend/src/components/NavigationButton.jsx
+++ frontend/src/components/NavigationButton.jsx
@@ -69,7 +69,7 @@ const NavigationButton = ({
   
   // Determine button text based on state
   const buttonText = clicked ? 
-    (isNext ? 'Navigating...' : 'Please wait...') : 
+    (isNext ? '...' : 'Please wait...') : 
     children;
   
   return (
